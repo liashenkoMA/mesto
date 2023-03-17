b567e229f891fc36fb51f0118c3fac9feb0a95e4
Maksim Liashenko <maliashenko@yandex.ru>
2023-03-17 17:16:26 +0400

    feat: написать скрипт

---
+++ blocks/profile/profile.js
@@ -0,0 +1,27 @@
+let editProfileButton = document.querySelector('.profile__edit-button')
+let popup = document.querySelector('.popup')
+let popupCloseButton = document.querySelector('.popup__button-close')
+
+let profileName = document.querySelector('.profile__title')
+let profileDescription = document.querySelector('.profile__description')
+let popupNameAdd = document.querySelector('.popup__text_type_title')
+let popupDescriptionAdd = document.querySelector('.popup__text_type_description')
+let popupSaveButton = document.querySelector('.popup__button_save')
+
+function popupOpen() {
+  popup.classList.add('popup__opened');
+};
+
+function popupClose() {
+  popup.classList.remove('popup__opened');
+};
+
+function saveInfoPopup() {
+  profileName.textContent = popupNameAdd.value;
+  profileDescription.textContent = popupDescriptionAdd.value;
+  popupClose()
+};
+
+editProfileButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
+popupSaveButton.addEventListener('click', saveInfoPopup);
